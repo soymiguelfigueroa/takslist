379a1c3634a5464e7c2e4314e6317bdab9d90f5e
Miguel Figueroa <miguelfigueroa90@gmail.com>
2023-08-02 16:09:23 -0400

    defining first controller callback in routes

--- html/index.php
+++ html/index.php
@@ -5,7 +5,5 @@ require_once 'vendor/autoload.php';
 use App\Routing\Web as WebRouter;
 
 $router = new WebRouter();
-$router->add('/', function () {
-    echo 'Hello Router!';
-});
+$router->add('/', 'App\Controllers\Controller@index');
 $router->run();
