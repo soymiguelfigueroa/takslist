113844e5414157c390462d92884539fbe4dded79
Miguel Figueroa <miguelfigueroa90@gmail.com>
2023-08-04 15:30:41 -0400

    Adding doctrin cli-config

--- html/bootstrap.php
+++ html/bootstrap.php
@@ -1,18 +1,16 @@
 <?php
-// bootstrap.php
+
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 
 require_once "vendor/autoload.php";
 
-// Create a simple "default" Doctrine ORM configuration for Attributes
 $config = ORMSetup::createAttributeMetadataConfiguration(
     paths: array(__DIR__."/app"),
     isDevMode: true,
 );
 
-// configuring the database connection
 $connection = DriverManager::getConnection([
     'driver' => 'pdo_mysql',
     'user' => 'root',
@@ -22,5 +20,4 @@ $connection = DriverManager::getConnection([
     'dbname' => 'takslist',
 ], $config);
 
-// obtaining the entity manager
 $entityManager = new EntityManager($connection, $config);
